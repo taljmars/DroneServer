b7a8ed0cf555aeefb088e9fb80235313af659f84
taljmars <taljmars@gmail.com>
2017-04-01 12:22:20 +0300

    update mission crud

--- Scheme/src/main/java/com/dronedb/persistence/scheme/CovertDatabaseVisited.java
+++
@@ -1,9 +0,0 @@
-package com.dronedb.persistence.scheme;
-
-/**
- * Created by taljmars on 3/18/17.
- */
-public interface CovertDatabaseVisited {
-
-    void accept(ConvertDatabaseVisitor convertDatabaseVisitor);
-}
