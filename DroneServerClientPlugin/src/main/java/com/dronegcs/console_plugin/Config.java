401b78b8bd4ad9cd52022d5713fd1086f134c8e1
taljmars <taljmars@gmail.com>
2017-05-27 09:34:21 +0300

    fix mission compiler

--- DroneServerClientPlugin/src/main/java/com/dronegcs/console_plugin/Config.java
+++ DroneServerClientPlugin/src/main/java/com/dronegcs/console_plugin/Config.java
@@ -1,6 +1,7 @@
 package com.dronegcs.console_plugin;
 
 import com.dronedb.persistence.ws.internal.*;
+import com.dronegcs.console_plugin.exceptions.ClientPluginException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +18,7 @@ public class Config {
 
     // DB Access
 
-    private static <T> T LoadServices(Class<T> clz) {
+    private static <T> T LoadServices(Class<T> clz) throws ClientPluginException {
         try {
             return getSrvicePort(clz, "178.62.1.156", 1234);
         }
@@ -31,40 +32,41 @@ public class Config {
         catch (Throwable e) {
             System.out.print("Failed to connect to the local database server, " + e.getMessage());
         }
-        return null;
+
+        throw new ClientPluginException("Failed to build connection to web service port");
     }
 
     private static <T> T getSrvicePort(Class<T> clz, String ipStr, int port) throws MalformedURLException {
         System.err.println("Got " + clz.getSimpleName());
         //URL url = new URL("http://localhost:9999/ws/" + clz.getSimpleName() + "?wsdl");
-        URL url = new URL("http://178.62.1.156:1234/ws/" + clz.getSimpleName() + "?wsdl");
+        URL url = new URL("http://" + ipStr + ":" + port + "/ws/" + clz.getSimpleName() + "?wsdl");
         QName qName = new QName("http://internal.ws.persistence.dronedb.com/", clz.getSimpleName() + "ImplService");
         Service service = Service.create(url, qName);
         return service.getPort(clz);
     }
 
     @Bean
-    public MissionCrudSvcRemote missionCrudSvcRemote() {
+    public MissionCrudSvcRemote missionCrudSvcRemote() throws ClientPluginException {
         return LoadServices(MissionCrudSvcRemote.class);
     }
 
     @Bean
-    public QuerySvcRemote querySvcRemote() {
+    public QuerySvcRemote querySvcRemote() throws ClientPluginException {
         return LoadServices(QuerySvcRemote.class);
     }
 
     @Bean
-    public DroneDbCrudSvcRemote droneDbCrudSvcRemote() {
+    public DroneDbCrudSvcRemote droneDbCrudSvcRemote() throws ClientPluginException {
         return LoadServices(DroneDbCrudSvcRemote.class);
     }
 
     @Bean
-    public PerimeterCrudSvcRemote perimeterCrudSvcRemote() {
+    public PerimeterCrudSvcRemote perimeterCrudSvcRemote() throws ClientPluginException {
         return LoadServices(PerimeterCrudSvcRemote.class);
     }
 
     @Bean
-    public SessionsSvcRemote sessionsSvcRemote() {
+    public SessionsSvcRemote sessionsSvcRemote() throws ClientPluginException {
         return LoadServices(SessionsSvcRemote.class);
     }
 
